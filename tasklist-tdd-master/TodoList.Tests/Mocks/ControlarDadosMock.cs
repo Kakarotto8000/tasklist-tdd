f719485c27e46ef1f45ef8ea25ff95b84b256ad9
zagoandre <andrezagosp@gmail.com>
2016-08-11 15:50:25 -0300

    esqueci os arquivos kkk

---
+++ tasklist-tdd-master/TodoList.Tests/Mocks/ControlarDadosMock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TodoList.Bussines;
+
+namespace TodoList.Tests.Mocks
+{
+    public class ControlarDadosMock : IControlarDados
+    {
+
+        private List<Task> ListaTarefas = new List<Bussines.Task>();
+        
+        public void Inserir(Bussines.Task tarefa)
+        {
+            tarefa.ID = ListaTarefas.Count + 1;
+            ListaTarefas.Add(tarefa);
+        }
+
+        public Task RecuperarPorId(int pintID)
+        {
+            return ListaTarefas.Single(x => x.ID.Equals(pintID));
+        }
+
+        public List<Bussines.Task> RecuperaTodasTarefas()
+        {
+            return ListaTarefas;
+        }
+
+        public bool verifica(Bussines.Task tarefa)
+        {
+            return ListaTarefas.Contains(tarefa);
+        }
+        
+    }
+}
---
+++ tasklist-tdd-master/TodoList.Tests/TaskTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoList.Bussines;
+using TodoList.Tests.Mocks;
+
+namespace TodoList.Tests
+{
+    [TestClass]
+    public class TaskTests
+    {
+
+        [TestMethod]
+        public void VerificarInsercaoTarefa()
+        {
+            //Arrange
+            var controlarDados = new ControlarDadosMock();
+            var tarefa = new Task("TESTE2");
+            
+            //Act
+            controlarDados.Inserir(tarefa);
+
+            //Assert
+            Assert.AreEqual(true, controlarDados.verifica(tarefa));            
+        }
+
+        //Arrange Act Assert
+
+        [TestMethod]
+        public void RecuperarTodasTarefas()
+        {
+            var controlar = new ControlarDadosMock();
+            var tarefa1 = new Task("TESTE1");
+            var tarefa2 = new Task("TESTE2");
+
+            controlar.Inserir(tarefa1);
+            controlar.Inserir(tarefa2);
+            
+            
+            var tarefas = controlar.RecuperaTodasTarefas();
+
+            Assert.AreEqual(2, tarefas.Count);
+
+            Assert.AreEqual(1, tarefas[0].ID);
+
+            Assert.AreEqual("TESTE1", tarefas[0].Name);
+
+            Assert.AreEqual(2, tarefas[1].ID);
+
+            Assert.AreEqual("TESTE2", tarefas[1].Name);
+        }
+
+        [TestMethod]
+        public void RecuperarTarefaPorID()
+        {
+            var lintID = 1;
+
+            var controlar = new ControlarDadosMock();
+
+            var tarefa = new Task("TESTE1");
+
+            controlar.Inserir(tarefa);
+
+            var tarefaRecuperada = controlar.RecuperarPorId(lintID);
+
+            Assert.AreEqual(1, tarefaRecuperada.ID);
+            Assert.AreEqual("TESTE1", tarefaRecuperada.Name);
+        }
+
+    }
+}
